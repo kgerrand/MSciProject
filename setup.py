dd36a6ebba1b2821f5c18be67cf3429f7df32c0e
Kirstin Gerrand <zj20898@bristol.ac.uk>
2024-01-10 11:37:03 +0000

    added metrics to benchmarks

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 site = 'MHD'
+site_dict = {"MHD":"Mace Head, Ireland", "BA":"Ragged Point, Barbados", "CG":"Cape Grim, Australia", "GS":"Gosan, South Korea",
+             "J1":"Jungfraujoch, Switzerland", "M5":"Monte Cimone, Italy", "TH":"Trinidad Head, USA", "ZE":"Zeppelin, Svalbard"}
 
 hfc_list = ["hfc-23", "hfc-32", "hfc-125", "hfc-134a", "hfc-143a", "hfc-152a", "hfc-227ea", "sf6"]
-compound = hfc_list[7]
+compound = hfc_list[3]
+
+confidence_threshold = 0.8
