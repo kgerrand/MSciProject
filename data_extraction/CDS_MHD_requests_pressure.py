0b5298901b7d01d896293b17600d9ee04ff2cdf2
Kirstin Gerrand <zj20898@bristol.ac.uk>
2024-01-26 14:10:14 +0000

    added functionality to skip over existing files (needing to run multiple times)

--- data_extraction/CDS_MHD_requests_pressure.py
+++ data_extraction/CDS_MHD_requests_pressure.py
@@ -10,6 +10,7 @@ This code is ran on BC4, a supercomputer at the University of Bristol.
 import cdsapi
 import sys
 from pathlib import Path
+import os
 
 c = cdsapi.Client()
 
@@ -22,58 +23,64 @@ names = ['Jan','Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'O
 def main(year):
 
     for n in range(len(months)):
-        print(f'Downloading {names[n]} {year}')
-           
-        c.retrieve(
-            'reanalysis-era5-pressure-levels',
-            {
-                'product_type': 'reanalysis',
-                'variable': 
-                [
-                    'u_component_of_wind', 'v_component_of_wind',
-                ],
-                
-                'pressure_level': 
-                [
-                    '500', '850',
-                ],
-                
-                'year': f'{year}',
-                'month': f'{months[n]}',
-                'day': 
-                [
-                    '01', '02', '03',
-                    '04', '05', '06',
-                    '07', '08', '09',
-                    '10', '11', '12',
-                    '13', '14', '15',
-                    '16', '17', '18',
-                    '19', '20', '21',
-                    '22', '23', '24',
-                    '25', '26', '27',
-                    '28', '29', '30',
-                    '31',
-                ],
+        output_filename = f'{output_path}/3dwind_{year}_{months[n]}.nc'
 
-                'time': 
-                [
-                    '00:00', '01:00', '02:00',
-                    '03:00', '04:00', '05:00',
-                    '06:00', '07:00', '08:00',
-                    '09:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00',
-                    '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00',
-                    '21:00', '22:00', '23:00',
-                ],
+        if os.path.exists(output_filename):
+            print(f"{names[n]} {year} already downloaded. Skipping.")
+
+        else:
+            print(f'Downloading {names[n]} {year}')
+           
+            c.retrieve(
+                'reanalysis-era5-pressure-levels',
+                {
+                    'product_type': 'reanalysis',
+                    'variable': 
+                    [
+                        'u_component_of_wind', 'v_component_of_wind',
+                    ],
                     
-                'format': 'netcdf',
-                'area': [60,-20, 40,0,],
-            },
-            # change the output file name as needed
-            f'{output_path}/3dwind_{year}_{months[n]}.nc')
-            
-        print(f'{names[n]} {year} downloaded')
+                    'pressure_level': 
+                    [
+                        '500', '850',
+                    ],
+                    
+                    'year': f'{year}',
+                    'month': f'{months[n]}',
+                    'day': 
+                    [
+                        '01', '02', '03',
+                        '04', '05', '06',
+                        '07', '08', '09',
+                        '10', '11', '12',
+                        '13', '14', '15',
+                        '16', '17', '18',
+                        '19', '20', '21',
+                        '22', '23', '24',
+                        '25', '26', '27',
+                        '28', '29', '30',
+                        '31',
+                    ],
+
+                    'time': 
+                    [
+                        '00:00', '01:00', '02:00',
+                        '03:00', '04:00', '05:00',
+                        '06:00', '07:00', '08:00',
+                        '09:00', '10:00', '11:00',
+                        '12:00', '13:00', '14:00',
+                        '15:00', '16:00', '17:00',
+                        '18:00', '19:00', '20:00',
+                        '21:00', '22:00', '23:00',
+                    ],
+                        
+                    'format': 'netcdf',
+                    'area': [60,-20, 40,0,],
+                },
+                # change the output file name as needed
+                output_filename)
+                
+            print(f'{names[n]} {year} downloaded')
        
 
 if __name__ == '__main__':
