4bb2890d44053797e5ee1aabd80d8d316faab9d0
Kirstin Gerrand <zj20898@bristol.ac.uk>
2024-01-28 18:06:34 +0000

    renamed setup.py to config.py and changed scripts accordingly

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ site = 'MHD'
 site_dict = {"MHD":"Mace Head, Ireland", "BA":"Ragged Point, Barbados", "CG":"Cape Grim, Australia", "GS":"Gosan, South Korea",
              "J1":"Jungfraujoch, Switzerland", "M5":"Monte Cimone, Italy", "TH":"Trinidad Head, USA", "ZE":"Zeppelin, Svalbard"}
 
-hfc_list = ["hfc-23", "hfc-32", "hfc-125", "hfc-134a", "hfc-143a", "hfc-152a", "hfc-227ea", "sf6"]
-compound = hfc_list[3]
+compound = "hfc-152a"
 
 confidence_threshold = 0.8
